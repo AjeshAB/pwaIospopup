efadd80b7616e002dcb750903a1cc5093d5a7458
Your Name <you@example.com>
2020-12-23 13:12:47 +0530

    first Push

---
+++ script.js
@@ -0,0 +1,52 @@
+function setCookie(name,value,days) {
+    var expires = "";
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "")  + expires + "; path=/";
+}
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
+}
+function eraseCookie(name) {
+    document.cookie = name +'=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
+
+
+
+$.getJSON('manifest.json',(json)=> {
+    window.document.getElementById('icon').style.backgroundImage = "url("+json.icons[0].src+")"
+})
+function Message() {
+    $("#Popups").hide()
+    setCookie("IosInstalled",true)
+}
+
+
+
+//if browser support service worker
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+}
+$("#Popups").hide()
+// Detects if device is on iOS
+const isIos = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+    getCookie("IosInstalled") ? $("#Popups").hide() : $("#Popups").show()
+}
